dd1601b5ddc2844760cdf02d64fb0d03e73e4c89
fugbot <18075133+fugbot@users.noreply.github.com>
2024-06-04 11:28:23 +0000

    Add styles for button and data- attribute

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const myCreatedLibrary = [
-    {
-        title: "Harry Potter",
-        author: "J.K. Rowling",
-        pages: 999,
-        read: true,
-    },
+// const myCreatedLibrary = [
+//     {
+//         title: "Harry Potter",
+//         author: "J.K. Rowling",
+//         pages: 999,
+//         read: true,
+//     },
 
-    {
-        title: "Animal Farm",
-        author: "George Orwell",
-        pages: 150,
-        read: true,
-    },
-    {
-        title: "Atlas Six",
-        author: "Olive Blake",
-        pages: 555,
-        read: false,
-    },
-];
+//     {
+//         title: "Animal Farm",
+//         author: "George Orwell",
+//         pages: 150,
+//         read: true,
+//     },
+//     {
+//         title: "Atlas Six",
+//         author: "Olive Blake",
+//         pages: 555,
+//         read: false,
+//     },
+// ];
 
 const myLibrary = [];
 
@@ -40,6 +40,8 @@ const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
 console.log(book1.getInfo());
 //addBookToLibrary();
 console.table(myLibrary);
+const svgRemove ='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" width="16px"><title>book-remove</title><path d="M13 19C13 20.1 13.3 21.12 13.81 22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.09C19.67 13.04 19.34 13 19 13C15.69 13 13 15.69 13 19M22.54 16.88L21.12 15.47L19 17.59L16.88 15.47L15.47 16.88L17.59 19L15.47 21.12L16.88 22.54L19 20.41L21.12 22.54L22.54 21.12L20.41 19L22.54 16.88Z" /></svg>';
+const svgContainer = document.querySelector(".svg-container");
 
 function addBookToLibrary() {
     let title = document.getElementById("book-title").value;
@@ -61,6 +63,7 @@ form.addEventListener("submit", (e) => {
     addBookToLibrary();
     printLastBookCard();
     form.reset();
+    closeDialog();
     //printLibraryCards();
 })
 
@@ -68,6 +71,10 @@ function printLastBookCard() {
     const bookCard = document.createElement("div")
     bookCard.className = "card";
     document.getElementById("books").appendChild(bookCard);
+
+    let indexNum = myLibrary.length-1;
+    console.log(indexNum);
+    bookCard.setAttribute("data-index-number", indexNum)
 
     const lastBook = myLibrary[myLibrary.length-1];
     //console.log("last book: " + JSON.stringify(lastBook));
@@ -87,12 +94,14 @@ function printLastBookCard() {
     const readDiv = document.createElement("div")
     readDiv.innerHTML = `Read?:  ${JSON.stringify(lastBook.read)} \n`;
     bookCard.appendChild(readDiv);
+
+    bookCard.appendChild(svgContainer);
+    //svgContainer.innerHTML = "hello";
+    //svgContainer.innerHTML = DOMPurify.sanitize(svgRemove);
+    svgContainer.innerHTML = svgRemove;
 }
 
 Object.setPrototypeOf(printLastBookCard.prototype, Book.prototype);
-
-
-
 
 function printLibraryCards() {
     myLibrary.forEach(function (book) {
@@ -136,6 +145,14 @@ newBtn.addEventListener("click", () => {
 })
 
 const closeBtn = document.querySelector(".close");
-closeBtn.addEventListener("click", () => {
+
+function closeDialog() {
     dialog.close();
-})
+}
+
+closeBtn.addEventListener("click", closeDialog);
+
+function removeBook(){
+        
+}
+svgRemove.addEventListener("click", removeBook);
