8ae1ecb4618d03ef39a5f068455200c1fd3df138
fugbot <18075133+fugbot@users.noreply.github.com>
2024-06-06 15:26:07 +0000

    Add basic read dropdown

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
 console.log(book1.getInfo());
 //addBookToLibrary();
 console.table(myLibrary);
-const svgRemove ='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" width="16px"><title>book-remove</title><path d="M13 19C13 20.1 13.3 21.12 13.81 22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.09C19.67 13.04 19.34 13 19 13C15.69 13 13 15.69 13 19M22.54 16.88L21.12 15.47L19 17.59L16.88 15.47L15.47 16.88L17.59 19L15.47 21.12L16.88 22.54L19 20.41L21.12 22.54L22.54 21.12L20.41 19L22.54 16.88Z" /></svg>';
+const svgRemove = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" width="16px"><title>book-remove</title><path d="M13 19C13 20.1 13.3 21.12 13.81 22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.09C19.67 13.04 19.34 13 19 13C15.69 13 13 15.69 13 19M22.54 16.88L21.12 15.47L19 17.59L16.88 15.47L15.47 16.88L17.59 19L15.47 21.12L16.88 22.54L19 20.41L21.12 22.54L22.54 21.12L20.41 19L22.54 16.88Z" /></svg>';
 
 function addBookToLibrary() {
     let title = document.getElementById("book-title").value;
@@ -71,11 +71,11 @@ function printLastBookCard() {
     bookCard.className = "card";
     document.getElementById("books").appendChild(bookCard);
 
-    let indexNum = myLibrary.length-1;
+    let indexNum = myLibrary.length - 1;
     console.log(indexNum);
     bookCard.setAttribute("data-index-number", indexNum)
 
-    const lastBook = myLibrary[myLibrary.length-1];
+    const lastBook = myLibrary[myLibrary.length - 1];
     //console.log("last book: " + JSON.stringify(lastBook));
 
     const titleDiv = document.createElement("div")
@@ -101,41 +101,65 @@ function printLastBookCard() {
     const functionContainer = document.createElement("div");
     functionContainer.className = "function-container";
     bookCard.appendChild(functionContainer);
-    
+
+    //dropdown select
+    const selectContainer = document.createElement("div");
+    functionContainer.appendChild(selectContainer);
+    selectContainer.classList = "select-container";
+
+    // const selectLabel = document.createElement("label");
+    // selectContainer.appendChild(selectLabel);
+    // selectLabel.setAttribute("for", "read-status");
+    // selectLabel.innerHTML = "Select Read Status: "
+
+    const select = document.createElement("select");
+    select.id="read-status";
+    select.setAttribute("name", "read-status");
+    selectContainer.appendChild(select);
+
+    const text = ["Select Read Status", "Read", "Want to Read", "DNF"];
+    for(i=0; i<text.length; i++) {
+        const readOpt = document.createElement("option");
+        readOpt.className = "status";
+        readOpt.setAttribute("value", [i]);
+        readOpt.innerHTML = text[i];
+        select.appendChild(readOpt);
+    }
+
+    //remove button
     removeBtnContainer = document.createElement("button");
     functionContainer.appendChild(removeBtnContainer);
     //functionContainer.innerHTML = DOMPurify.sanitize(svgRemove);
     removeBtnContainer.innerHTML = svgRemove;
 
-    //read status dropdown
-    const readDropdownContainer = document.createElement("div");
-    readDropdownContainer.classList = "dropdown";
-    functionContainer.appendChild(readDropdownContainer);
-    
-    const readDropdownBtn = document.createElement("button");
-    readDropdownBtn.classList = "readDropBtn";
-    readDropdownBtn.innerHTML = "Read Status";
-    readDropdownContainer.appendChild(readDropdownBtn);
-
-    const readDropdownDiv = document.createElement("div");
-    readDropdownDiv.id = "readDropdown";
-    readDropdownBtn.appendChild(readDropdownDiv);
-
-    const text = ["Read", "Want to Read", "DNF"];
-    text.forEach(function(el) {
-        const readOpt = document.createElement("a");
-        readOpt.className = "status";
-        readOpt.innerHTML = el;
-        readDropdownDiv.appendChild(readOpt);
-    })
 }
 
-function createReadDropdown() {
+// function createReadDropdown() {
+// //read status dropdown
+// const readDropdownContainer = document.createElement("div");
+// readDropdownContainer.classList = "dropdown";
+// functionContainer.appendChild(readDropdownContainer);
 
+// const readDropdownBtn = document.createElement("button");
+// readDropdownBtn.classList = "readDropBtn";
+// readDropdownBtn.setAttribute("onclick", "toggleDropdown()")
+// readDropdownBtn.innerHTML = "Read Status";
+// readDropdownContainer.appendChild(readDropdownBtn);
 
-    const readDropdown = document.createElement("button");
-    functionContainer.appendChild(readDropdown);
-}
+// const readDropdownDiv = document.createElement("div");
+// readDropdownDiv.id = "readDropdown";
+// readDropdownDiv.classList = "dropdown-content";
+// readDropdownBtn.appendChild(readDropdownDiv);
+
+// const text = ["Read", "Want to Read", "DNF"];
+// text.forEach(function(el) {
+//     const readOpt = document.createElement("a");
+//     readOpt.className = "status";
+//     readOpt.innerHTML = el;
+//     readDropdownDiv.appendChild(readOpt);
+// })
+
+// }
 
 Object.setPrototypeOf(printLastBookCard.prototype, Book.prototype);
 
@@ -178,7 +202,7 @@ function closeDialog() {
 
 closeBtn.addEventListener("click", closeDialog);
 
-function removeBook(){
+function removeBook() {
     clickedBookCard = event.target.parentNode;
     console.log(clickedBookCard);
     //clickedBookCard.remove();
@@ -187,36 +211,14 @@ function removeBook(){
 
 document.addEventListener("click", (e) => {
     //e.preventDefault();
-    if(!e.target.closest(".function-container>button")) return;
+    if (!e.target.closest(".function-container>button")) return;
     clickedBookCard = e.target.closest(".card");
     console.log(clickedBookCard);
     if (confirm('Are you sure you want to remove this book?')) {
         clickedBookCard.remove();
         console.log('Thing was removed.');
-      } else {
+    } else {
         // Do nothing!
         console.log('Thing was not saved to the database.');
-      }
-}, false);
-
-
-//dropdown read status
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
     }
-  } 
+}, false);
